45bbd80e315240a7c9912167ddcfb0cc5cf4b979
YoelUb <yurqubar@myuax.com>
2024-10-22 13:17:59 +0200

    Clase Empleado

---
+++ src/consultoria/Empleado.java
@@ -0,0 +1,69 @@
+package consultoria;
+
+import java.util.GregorianCalendar;
+
+public class Empleado {
+
+    protected String dni;
+    protected String nombre;
+    protected String apellido;
+    protected double sueldo;
+    protected GregorianCalendar fechaContratacion;
+
+    // Consrtuctor
+    public Empleado(String dni, String nombre, String apellido, double sueldo, GregorianCalendar fechaContratacion) {
+
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sueldo = sueldo;
+        this.fechaContratacion = fechaContratacion;
+
+    }
+
+    //Getters and Setters
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public GregorianCalendar getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(GregorianCalendar fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    @Override
+    public String toString() {
+
+
+        return String.format("DNI: " + dni + "\nNombre de empleado: " + nombre + "\nApellido: " + apellido + "\nSueldo: " + sueldo + " €" + "\nFecha de Contratacacion: " + fechaContratacion.getTime());
+
+    }
+
+
+}
+
--- src/consultoria/Main.java
+++ src/consultoria/Main.java
@@ -1,9 +1,15 @@
 package consultoria;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        GregorianCalendar gc = new GregorianCalendar(2023, Calendar.JANUARY, 12);
+        Empleado yoel = new Empleado("123456789A", "Yoel", "Urquijo", 2000, gc);
+
+        System.out.println(yoel);
 
 
     }
