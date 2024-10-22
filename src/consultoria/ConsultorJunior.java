6606a43a8a8f4106c9aeb25db8f1022666a8f8fc
YoelUb <yurqubar@myuax.com>
2024-10-22 16:50:52 +0200

    Clase Consultor Junior

--- src/consultoria/ConsultorJunior.java
+++ src/consultoria/ConsultorJunior.java
@@ -1,8 +1,47 @@
 package consultoria;
 
+import javax.management.remote.SubjectDelegationPermission;
+import java.util.GregorianCalendar;
+
 public class ConsultorJunior extends Consultor {
 
 
+    protected ConsultorSenior supervisor;
+    protected boolean ascenso;
+
+
+    public ConsultorJunior(String dni, String nombre, String apellido, double sueldo, GregorianCalendar fechaContratacion, ConsultorSenior supervisor, boolean ascenso) {
+        super(dni, nombre, apellido, sueldo, fechaContratacion);
+
+        this.supervisor = supervisor;
+        this.ascenso = false;
+
+    }
+
+
+    public ConsultorSenior getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(ConsultorSenior supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public boolean isAscenso() {
+        return ascenso;
+    }
+
+    public void setAscenso(boolean ascenso) {
+        this.ascenso = ascenso;
+    }
+
+
+    public void cambiarSupervisor(ConsultorSenior nuevoSupervisor) {
+
+        supervisor = nuevoSupervisor;
+
+
+    }
 
 
 }
