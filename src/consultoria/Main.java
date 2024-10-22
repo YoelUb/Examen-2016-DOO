5206b0b3ad166edba84a3ec6bc756889658e23a8
YoelUb <yurqubar@myuax.com>
2024-10-22 13:33:35 +0200

    Clase Directivo

--- src/consultoria/Main.java
+++ src/consultoria/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
         GregorianCalendar gc = new GregorianCalendar(2023, Calendar.JANUARY, 12);
         Empleado yoel = new Empleado("123456789A", "Yoel", "Urquijo", 2000, gc);
-
+        Directivo marta = new Directivo("123456749A", "Marta", "Rodriguez", 2000, gc, "Directiva");
+        System.out.println("\n----------------------------------------------------------");
         System.out.println(yoel);
-
+        System.out.println(marta);
 
 
     }
